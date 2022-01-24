c9d78ddd4905e06b7d50e5473001c0db3dfcfaf1
dzavi84 <milanvidanovic@yahoo.com>
2022-01-24 18:10:59 +0100

    commitlastPM

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,34 @@ import styled from 'styled-components';
 const Header = (props) => {
   return (
     <Container>
-      <Content>Header</Content>
+      <Content>
+        <Logo>
+          <a href='/home'>
+            <img src='/images/Tigropolis_logo.png' />
+          </a>
+        </Logo>
+      </Content>
     </Container>
   );
 };
 
-const Container = styled.div``;
-const Content = styled.div``;
+const Container = styled.div`
+  background-color: white;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+  left: 0;
+  padding: 0 24px;
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  z-index: 100;
+`;
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+  min-height: 100%;
+  max-width: 1128px;
+`;
+const Logo = styled.div``;
 
 export default Header;
