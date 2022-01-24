7eb46b0a64d512333d11c9824c7334a618dadacd
dzavi84 <milanvidanovic@yahoo.com>
2022-01-24 16:23:26 +0100

    commit1

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,18 +5,24 @@ const Login = (props) => {
     <Container>
       <Nav>
         <a href='/'>
-          <img src='/images/Tigropolis_login-page.png' />
+          <img src='/images/Tigropolis_login-page21212.png' />
         </a>
         <div>
           <Join>Join Now!</Join>
-          <Singin>Sign In!</Singin>
+          <SignIn>Sign In!</SignIn>
         </div>
       </Nav>
       <Section>
         <Hero>
           <h1>Welcome to your login page! </h1>
-          <img src='/images/Vecteezy_Geometric-Background_Revisi2-Revisi_RW0320.jpg' />
+          <img src='/images/44868c36.svg' />
         </Hero>
+        <Form>
+          <Google>
+            <img src='/images/google.svg' />
+            Sign In With Google
+          </Google>
+        </Form>
       </Section>
     </Container>
   );
@@ -25,45 +31,42 @@ const Login = (props) => {
 const Container = styled.div`
   padding: 0px;
 `;
-const Nav = styled.div`
+
+const Nav = styled.nav`
   max-width: 1128px;
   margin: auto;
   padding: 12px 0 16px;
-  display:flex;
-  align-items:top;
-  position:relative;
-  justify-content:space-between;
-  flex-wrap:nowrap;
-
-
-  b>a(
-      width:135px;
-      height:34px;
-      @media screen and(max-width:768px){
-          padding:0 5px;
-      }
-
-
-  )
-
+  display: flex;
+  align-items: center;
+  position: relative;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+  & > a {
+    width: 135px;
+    height: 34px;
+    @media (max-width: 768px) {
+      padding: 0 5px;
+    }
+  }
 `;
+
 const Join = styled.a`
-font-size:16px;
-padding:10px 12px;
-text-decoration:none;
-border-radius:4px;
-color:rgba(0,0,0,0.6);
-margin-right:12;
-&:hover{
-    background-color:rgba(0,0,0,0.08);
-    color:rgba(0,0,0,0.6)
-    text-decoration:none;
-
-}
+  font-size: 16px;
+  padding: 10px 12px;
+  text-decoration: none;
+  border-radius: 4px;
+  color: rgba(0, 0, 0, 0.6);
+  margin-right: 12px;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+    color: rgba(0, 0, 0, 0.9);
+    text-decoration: none;
+  }
 `;
-const Singin = styled.a`
-  box-shadow: inset 0 0 0 1px #ff3131;
-  color: #ff3131;
+
+const SignIn = styled.a`
+  box-shadow: inset 0 0 0 1px #0a66c2;
+  color: #0a66c2;
   border-radius: 24px;
   transition-duration: 167ms;
   font-size: 16px;
@@ -73,11 +76,12 @@ const Singin = styled.a`
   text-align: center;
   background-color: rgba(0, 0, 0, 0);
   &:hover {
-    background-color: rgba(255, 7, 58, 0.15);
-    color: #ff3131;
+    background-color: rgba(112, 181, 249, 0.15);
+    color: #0a66c2;
     text-decoration: none;
   }
 `;
+
 const Section = styled.section`
   display: flex;
   align-content: start;
@@ -86,48 +90,75 @@ const Section = styled.section`
   padding-top: 40px;
   padding: 60px 0;
   position: relative;
-  flax-wrap: wrap;
+  flex-wrap: wrap;
   width: 100%;
   max-width: 1128px;
   align-items: center;
   margin: auto;
-  @media screen and (max-width: 768px) {
+  @media (max-width: 768px) {
     margin: auto;
     min-height: 0px;
   }
 `;
-const Hero = styled.div`
-  width: 80%;
 
+const Hero = styled.div`
+  width: 100%;
   h1 {
-    text-align: center;
     padding-bottom: 0;
-    width: 75%;
+    width: 55%;
     font-size: 56px;
-    color: rgba(255, 7, 58, 0.35);
+    color: #2977c9;
     font-weight: 200;
     line-height: 70px;
-    @media screen and (max-width: 768px) {
+    @media (max-width: 768px) {
       text-align: center;
       font-size: 20px;
       width: 100%;
       line-height: 2;
     }
   }
-
   img {
-    z-index: -1;
+    border-radius: 50%;
     width: 700px;
     height: 670px;
     position: absolute;
     bottom: -2px;
     right: -150px;
-    @media screen and (max-width: 768px) {
+    @media (max-width: 768px) {
       top: 230px;
-      width: -webkit-fill-available;
+      width: initial;
       position: initial;
       height: initial;
     }
+  }
+`;
+
+const Form = styled.div`
+  margin-top: 100px;
+  width: 408px;
+  @media (max-width: 768px) {
+    margin-top: 20px;
+  }
+`;
+
+const Google = styled.button`
+  display: flex;
+  justify-content: center;
+  background-color: #fff;
+  align-items: center;
+  height: 56px;
+  width: 100%;
+  border-radius: 28px;
+  box-shadow: inset 0 0 0 1px rgb(0 0 0 / 60%),
+    inset 0 0 0 2px rgb(0 0 0 / 0%) inset 0 0 0 1px rgb(0 0 0 / 0);
+  vertical-align: middle;
+  z-index: 0;
+  transition-duration: 167ms;
+  font-size: 20px;
+  color: rgba(0, 0, 0, 0.6);
+  &:hover {
+    background-color: rgba(207, 207, 207, 0.25);
+    color: rgba(0, 0, 0, 0.75);
   }
 `;
 
