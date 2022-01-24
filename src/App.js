91e12065671b2017930f11299507a4be007c7caf
dzavi84 <milanvidanovic@yahoo.com>
2022-01-24 16:37:34 +0100

    commit2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import './App.css';
+import Header from './components/Header';
 import Login from './components/Login';
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Login />} />
         </Routes>
+        <Route path='/Home'>
+          <Header />
+        </Route>
       </Router>
     </div>
   );
