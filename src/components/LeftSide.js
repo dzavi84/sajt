c56a8614368f53fb44239e931cc4ce28bce5c28e
dzavi84 <milanvidanovic@yahoo.com>
2022-01-29 12:57:21 +0100

    commit2

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -47,8 +47,33 @@ const CardBackground = styled.div`
   height: 54px;
   margin: -12px -12px 0;
 `;
-const Photo = styled.div``;
-const Link = styled.div``;
-const AddPhotoText = styled.div``;
+const Photo = styled.div`
+  box-shadow: none;
+  background-image: url(/images/photo.svg);
+  width: 72px;
+  height: 72px;
+  box-sizing: border-box;
+  background-clip: content-box;
+  background-color: white;
+  background-position: center;
+  background-size: 60%;
+  background-repeat: no-repeat;
+  border: 2px solid white;
+  margin: -38px auto 12px;
+  border-radius: 50%;
+`;
+const Link = styled.div`
+  font-size: 16px;
+  line-height: 1.5;
+  color: rgba(0, 0, 0, 0.9);
+  font-weight: 600;
+`;
+const AddPhotoText = styled.div`
+  color: #0a66c2;
+  margin-top: 4px;
+  font-size: 12px;
+  line-height: 1.33;
+  font-weight: 400;
+`;
 
 export default LeftSide;
