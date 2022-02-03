f21028ccdeec0c4dd60e82d079caf4711a78ba41
dzavi84 <milanvidanovic@yahoo.com>
2022-02-03 21:22:06 +0100

    commit2

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,31 @@
 import styled from 'styled-components';
 
 const Main = (props) => {
-  return <Container>Main</Container>;
+  return (
+    <Container>
+      <ShareBox>Share</ShareBox>
+    </Container>
+  );
 };
 
 const Container = styled.div`
   grid-area: Main;
 `;
 
+const CommentCard = styled.div`
+  text-align: center;
+  margin-bottom: 8px;
+  overflow: hidden;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0/15%), 0 0 0 rgb(0 0 0 /20%);
+`;
+const ShareBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #958b7b;
+  background: white;
+`;
 export default Main;
